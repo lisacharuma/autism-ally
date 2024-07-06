4a4de2b949eb39a86ebc6d62a6ac1f3c3c459d5f
lisacharuma <charumalisah@gmail.com>
2024-07-06 13:31:55 +0200

    Added an application factory to the app

---
+++ api/db.py
@@ -0,0 +1,5 @@
+# Database configuration
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for, jsonify
+from flask_bcrypt import Bcrypt  # Import bcrypt for pswd hashing
+from api.db import db        #imports the database setup
+from api.models import ma
+from flask_cors import CORS  # Import the CORS module
+import os
 
-app=Flask(__name__)
+
+# the application factory
+def create_app():
+    app=Flask(__name__)
+    app.secret_key = "AutismAlly"
+    return app
+
 
 @app.route('/')
 def hello():

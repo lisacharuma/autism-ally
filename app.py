a68e56acc435550d9512cdfce2c3ba2ca894a9f8
lisacharuma <charumalisah@gmail.com>
2024-07-06 12:57:08 +0200

    Working test app

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 
-app=flask(__app__)
+app=Flask(__name__)
 
 @app.route('/')
 def hello():
-    return ("Hello World!")
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
